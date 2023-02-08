3fbade3675fe8adaf95f182e381edca25a7e61c6
Guilherme0204 <gui2605costa@gmail.com>
2023-02-07 23:04:37 -0300

    index.htlm incremented and script.js incremented

--- script.js
+++ script.js
@@ -1,20 +1,23 @@
-const form = document.getElementById("contact-form")
+const form = document.getElementById("contact-form");
+
+function register(name, email, message) {
+    const nome = form.elements["name"].value;
+    const mensagem = form.elements["message"].value;
+    if (nome.length === 0) {
+        alert("Nome obrigatorio");
+        return
+    } else if (mensagem.length === 0) {
+        alert("Mensagem obrigatoria")
+        return
+    } else {
+        alert(`Nome: ${name} Email: ${email} Mensagem: ${message}`);
+    }
+}
 form.addEventListener("submit", (e) => {
-    e.preventDefault()
-    const name = form.elements["name"].value
-    if (name.length === 0) {
-        alert("Nome obrigatório");
-        return;
-    }
-    const email = form.elements["email"].value
-    const message = form.elements["message"].value
-    if (message.length === 0) {
-        alert("Mensagem obrigatório");
-        return;
-    }
-    alert(`Nome: ${name} Email: ${email} Mensagem: ${message}`)
+    e.preventDefault();
+    const name = form.elements["name"].value;
+    const email = form.elements["email"].value;
+    const message = form.elements["message"].value;
+    register(name, email, message)
 
-
-
-
-})
+});
